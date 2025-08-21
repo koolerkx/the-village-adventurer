6d385ed760e2d4bab2d350c4093835f9695cd6a9
Kooler Fan <koolerkx@gmail.com>
2025-08-22 07:00:39 +0900

    ui: implement hp bar
    refs #44

--- Game/ui/game_ui.cpp
+++ Game/ui/game_ui.cpp
@@ -3,13 +3,78 @@ module;
 module game.ui.game_ui;
 
 import std;
+import graphic.utils.types;
+import graphic.utils.font;
 
 GameUI::GameUI(GameContext* ctx, SceneContext* scene_ctx, std::wstring texture_path) {
   texture_id_ = ctx->render_resource_manager->texture_manager->Load(texture_path);
+
+  auto [font_spritemap_filename, font_metadata_filename] = defined_font_map[DefinedFont::FUSION_PIXEL_FONT_DEBUG];
+  font_key_ = Font::MakeFontGetKey(font_spritemap_filename, font_metadata_filename,
+                                   ctx->render_resource_manager->texture_manager.get());
+  default_font_ = Font::GetFont(font_key_);
 }
 
 void GameUI::OnUpdate(GameContext* ctx, SceneContext* scene_ctx, float delta_time) {}
 
 void GameUI::OnFixedUpdate(GameContext* ctx, SceneContext* scene_ctx, float delta_time) {}
 
-void GameUI::OnRender(GameContext* ctx, SceneContext* scene_ctx, Camera* camera) {}
+void GameUI::OnRender(GameContext* ctx, SceneContext* scene_ctx, Camera* camera) {
+  auto& rr = ctx->render_resource_manager->renderer;
+  std::wstringstream wss;
+
+  std::vector<RenderInstanceItem> render_items;
+
+  // Session: Left Upper
+  // HP Bar: Background
+  render_items.emplace_back(RenderInstanceItem{
+    Transform{
+      .position = {24, 24, 0},
+      .size = {320, 64},
+    },
+    texture_map["HPBarPanel"], color::white
+  });
+  // HP Bar: Heart Background Circle
+  render_items.emplace_back(RenderInstanceItem{
+    Transform{
+      .position = {28, 20, 0},
+      .size = {72, 72},
+    },
+    texture_map["BigCircle"], color::white
+  });
+  // HP Bar: Heart
+  render_items.emplace_back(RenderInstanceItem{
+    Transform{
+      .position = {38, 30, 0},
+      .size = {52, 52},
+    },
+    texture_map["Heart"], color::white
+  });
+  // HP Bar: HB Bar
+  float hp_percent = 1.0f;
+  render_items.emplace_back(RenderInstanceItem{
+    Transform{
+      .position = {110, 59, 0},
+      .size = {200 / hp_percent, 14},
+    },
+    texture_map["HPBar"], color::white
+  });
+  // HP Bar: HB Bar Frame
+  render_items.emplace_back(RenderInstanceItem{
+    Transform{
+      .position = {106, 54, 0},
+      .size = {208, 24},
+    },
+    texture_map["HPBarFrame"], color::white
+  });
+  // On Top of instanced Draw
+  // Session: Left Upper
+  // HP Bar: Player Name
+  rr->DrawFont(L"プレイヤー", font_key_,
+               Transform{.position = {110, 36, 0}},
+               StringSpriteProps{
+                 .pixel_size = 14.0f,
+                 .letter_spacing = 0.0f,
+                 .line_height = 0.0f
+               });
+}
--- Game/ui/game_ui.ixx
+++ Game/ui/game_ui.ixx
@@ -7,11 +7,24 @@ import game.context;
 import game.scene_game.context;
 import game.scene_object.camera;
 import graphic.utils.fixed_pool;
+import graphic.utils.font;
+import graphic.utils.types;
 
+std::unordered_map<std::string, UV> texture_map = {
+  // HP Bar
+  {"HPBarPanel", UV{{384, 0}, {160, 41}}},
+  {"BigCircle", UV{{384, 41}, {41, 41}}},
+  {"Heart", UV{{508, 41}, {32, 32}}},
+  {"HPBar", UV{{224, 128}, {120, 8}}},
+  {"HPBarFrame", UV{{224, 112}, {128, 16}}},
+
+};
 export class GameUI {
 private:
   FixedPoolIndexType texture_id_;
+  std::wstring font_key_;
 
+  Font* default_font_;
 public:
   GameUI(GameContext* ctx, SceneContext* scene_ctx, std::wstring texture_path);
   void OnUpdate(GameContext* ctx, SceneContext* scene_ctx, float delta_time);
