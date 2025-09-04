e3c29889314c7804a1828672ce0f911321e2ce00
Kooler Fan <koolerkx@gmail.com>
2025-09-05 06:05:14 +0900

    input: add xinput for title scene
    refs #114

--- Game/scene/title_scene.cpp
+++ Game/scene/title_scene.cpp
@@ -19,8 +19,14 @@ void TitleScene::OnUpdate(GameContext* ctx, float delta_time) {
   title_ui_->OnUpdate(ctx, delta_time);
   auto am = SceneManager::GetInstance().GetAudioManager();
 
+  auto& ih = ctx->input_handler;
+
+  bool is_xinput_button_yes = ih->GetXInputButton(XButtonCode::A);
+  bool is_xinput_button_up = ih->GetXInputButton(XButtonCode::DPadUp) || ih->GetXInputAnalog().first.second > 0.0f;
+  bool is_xinput_button_down = ih->GetXInputButton(XButtonCode::DPadDown) || ih->GetXInputAnalog().first.second < 0.0f;
+
   if (is_allow_control_) {
-    if ((ctx->input_handler->GetKey(KeyCode::KK_UP) || ctx->input_handler->GetKey(KeyCode::KK_W))
+    if ((ctx->input_handler->GetKey(KeyCode::KK_UP) || ctx->input_handler->GetKey(KeyCode::KK_W) || is_xinput_button_up)
       && input_throttle_.CanCall()) {
       selected_option_++;
       selected_option_ %= static_cast<uint8_t>(options_count);
@@ -28,7 +34,7 @@ void TitleScene::OnUpdate(GameContext* ctx, float delta_time) {
       am->PlayAudioClip(audio_clip::keyboard_click, {0, 0}, 0.25);
     }
 
-    if ((ctx->input_handler->GetKey(KeyCode::KK_DOWN) || ctx->input_handler->GetKey(KeyCode::KK_S))
+    if ((ctx->input_handler->GetKey(KeyCode::KK_DOWN) || ctx->input_handler->GetKey(KeyCode::KK_S) || is_xinput_button_down)
       && input_throttle_.CanCall()) {
       selected_option_--;
       selected_option_ += static_cast<uint8_t>(options_count);
@@ -37,7 +43,7 @@ void TitleScene::OnUpdate(GameContext* ctx, float delta_time) {
       am->PlayAudioClip(audio_clip::keyboard_click, {0, 0}, 0.25);
     }
 
-    if ((ctx->input_handler->IsKeyDown(KeyCode::KK_ENTER) || ctx->input_handler->IsKeyDown(KeyCode::KK_SPACE))
+    if ((ctx->input_handler->IsKeyDown(KeyCode::KK_ENTER) || ctx->input_handler->IsKeyDown(KeyCode::KK_SPACE) || is_xinput_button_yes)
       && enter_throttle_.CanCall()) {
       if (static_cast<SelectedOption>(selected_option_) == SelectedOption::START_GAME) {
         am->PlayAudioClip(audio_clip::equip_3, {0, 0}, 0.75);
